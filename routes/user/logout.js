c92b2c1e4c6d3e54e1007be2bc2896a9a2a5e84e
x1x0 <hgliguroski@gmail.com>
2022-12-18 18:03:40 +0100

    fixed issue

--- routes/user/logout.js
+++ routes/user/logout.js
@@ -14,7 +14,7 @@ Router.post('/', async (req, res) => {
         const result = jwt.verify(req.body.token || req.cookies.token, req.app.get("secret"), {algorithms: ["RS256"]})
         if(parseInt(`${result.exp}000`) < Date.now())
             return res.send({error: true, code: 401, message: "JWT_EXPIRED"})
-        await global.redis.lpush("invalid_tokens", `${req.body.token},${result.exp}000`)
+        await global.redis.lpush("invalid_tokens", `${req.body.token || req.cookies.token},${result.exp}000`)
         res.clearCookie('token');
         res.send({error: false, status: "SUCCESS"});
         const lindex = await global.redis.lindex("invalid_tokens", -1);
