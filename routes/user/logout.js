17a45c8a0ad4a40be2d34409bfce58ade4f094d6
x1x0 <hgliguroski@gmail.com>
2022-12-19 19:37:02 +0100

    checking expire date before redis

--- routes/user/logout.js
+++ routes/user/logout.js
@@ -6,9 +6,9 @@ Router.post('/', async (req, res) => {
     try {
         if(!req.body.token && !req.cookies.token) return res.send({error: true, code: 401, message: "UNAUTHORIZED"})
         const result = jwt.verify(req.body.token || req.cookies.token, req.app.get("secret"), {algorithms: ["RS256"]})
-        if(await checkForInvalidToken(result)) return res.send({error: true, code: 401, message: "INVALID_TOKEN"})
         if(parseInt(`${result.exp}000`) < Date.now())
             return res.send({error: true, code: 401, message: "JWT_EXPIRED"})
+        if(await checkForInvalidToken(result)) return res.send({error: true, code: 401, message: "INVALID_TOKEN"})
         await global.redis.zadd("logouts", Date.now(), `${result.id}`)
         res.clearCookie('token');
         if(req.body.redirect_url)
