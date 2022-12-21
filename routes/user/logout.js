4aeba2baa9c27671412552da81515b4e5a3b7c80
x1x0 <hgliguroski@gmail.com>
2022-12-21 22:28:43 +0100

    removed debug

--- routes/user/logout.js
+++ routes/user/logout.js
@@ -4,7 +4,6 @@ const Router = require('express').Router()
 
 Router.post('/', async (req, res) => {
     try {
-        console.log(req.body.token, req.cookies.token)
         if(!req.body.token && !req.cookies.token) return res.send({error: true, code: 401, message: "UNAUTHORIZED"})
         const result = jwt.verify(req.body.token || req.cookies.token, req.app.get("secret"), {algorithms: ["RS256"]})
         if(parseInt(`${result.exp}000`) < Date.now())
